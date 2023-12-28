622f996ca4a1f7067b8a9424570b76ee5dd89c63
Manas <chilamwarmanas@gmail.com>
2023-12-28 12:01:08 +0530

    first commit

---
+++ index.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // This is a simple C++ program
+    cout << "Hello, World!" << endl;
+
+    // The program returns 0 to indicate successful execution
+    return 0;
+}
